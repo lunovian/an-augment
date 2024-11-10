12ca8c8b9a19e0baa4eed7a2dc506e5f89b5f367
anmatngu <nxan2911@gmail.com>
2024-11-10 17:53:40 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='MedAugment',
+    version='0.1',
+    description='A Medical Imaging Augmentation Library',
+    author='lunovian',
+    license='Apache-2.0',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=['numpy', 'scipy', 'opencv-python'],
+)
