bc556fa6f92a3c688378b96005e77f5342102179
anmatngu <nxan2911@gmail.com>
2024-11-19 00:25:08 +0700

    Refactor module imports to use relative paths; update __all__ to reflect the new structure

--- src/an_augment/__init__.py
+++ src/an_augment/__init__.py
@@ -1,21 +1,24 @@
-# Expose the module
-from default.noise import noise
-from default.blur import blur
-from default.crop import crop
-from default.elastic_deformation import elastic_deformation
-from default.flip import flip
-from default.intensity import intensity
-from default.occlusion import occlusion
-from default.random_crop import random_crop
-from default.random_rotation import random_rotation
-from default.rotate import rotate
-from default.scale import scale
-# from .environmental import *
-# from .mechanical import *
-# from .medical import *
+from .default.blur import blur
+from .default.crop import crop
+from .default.elastic_deformation import elastic_deformation
+from .default.flip import flip
+from .default.intensity import intensity
+from .default.noise import noise
+from .default.occlusion import occlusion
+from .default.random_rotation import random_rotation
+from .default.rotate import rotate
+from .default.scale import scale
 
 # Define all accessible modules and functions
-__all__ = ["noise", "blur", "crop",
-           "elastic_deformation", "flip", "intensity",
-           "occlusion", "random_crop", "random_rotation",
-           "rotate", "scale"]
+__all__ = [
+    "blur",
+    "crop",
+    "elastic_deformation",
+    "flip",
+    "intensity",
+    "noise",
+    "occlusion",
+    "random_rotation",
+    "rotate",
+    "scale"
+]
