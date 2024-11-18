c1ba703aa002ed78a5339e0657863e80b2e408f9
anmatngu <nxan2911@gmail.com>
2024-11-19 02:17:37 +0700

    Update setup.py: modify package description for clarity and consistency

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="anaug",
     version="0.1.0",
-    description="AnAugment: A Python library for diverse data augmentation.",
+    description="An-Augment: A Python library for diverse data augmentation.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="lunovian",
