092a2a8118291aecb6f538cc557d53b90368642a
anmatngu <nxan2911@gmail.com>
2024-11-23 17:58:25 +0700

    Bump version to 0.1.1 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="anaug",
-    version="0.1.0",
+    version="0.1.1",
     description="AnAugment: A Python library for diverse data augmentation.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
